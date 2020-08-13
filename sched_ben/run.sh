45ca9723aa6f241307e63ef1feb2c73081948d52
WenyuanShao <wenyuan@SEAS15672.seas.gwu.edu>
2020-08-13 12:26:03 -0400

    result

--- sched_ben/run.sh
+++ sched_ben/run.sh
@@ -5,7 +5,7 @@ do
 	mkdir logs
 	for sets in fprr bitmap  rbtree
 	do	
-		for thds in 64 128 256 512 1024
+		for thds in 2 4 8 16 32 64 128 256 512 1024
 		do	
 			cd ${cdir}
 			./linux_ben $sets $thds
@@ -13,7 +13,7 @@ do
 			python ../parse.py $sets$thds.log > $sets$thds.data
 			cat ${sets}$thds.data | tr -d 'a-z' >| $thds.data
 		done
-		paste -d: ${sets}64.data 128.data 256.data 512.data 1024.data >> output$num.data
+		paste -d: ${sets}2.data 4.data 8.data 32.data 64.data 128.data 256.data 512.data 1024.data >> output$num.data
 	done
 	cd ${cdir}
 	mv logs logs$num
